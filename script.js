92d09869ff4f274f576048e4d0e46a36247735cf
Aditya Divyang <aditya.divyang69@gmail.com>
2021-05-24 20:18:56 +0530

    added maps

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+const Geolocation = navigator.geolocation;
+
+const success = function (position) {
+  const { latitude, longitude } = position.coords;
+  console.log(`https://www.google.pt/maps/@${latitude},${longitude}`);
+
+  const coords = [latitude, longitude];
+
+  const map = L.map('map').setView(coords, 15);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  L.marker(coords)
+    .addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+};
+const failure = function () {
+  alert(`Could not get your coordinates`);
+};
+Geolocation.getCurrentPosition(success, failure);
